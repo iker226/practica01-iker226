ed523e60d28befe7c1477545f93b4f29d9a4f51c
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:39:09 -0600

    Update ej5.c

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 5
+    
+    int a[10];
+    
+    printf("&a[0] = %p\n", &a[0]);
+    printf("&a[10] = %p\n", &a[10]);
+    
     return 0;
 }
