a1e83f7a41c0a257f066c4c32bb7e91f051d94f4
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:37:37 -0600

    Update ej3.c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -2,5 +2,21 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 3
+    int a[3];
+    int b[3];
+    int c[3];
+
+    for(int i=0; i<6; i++){
+        printf("La direccion de a[%i] es: %p\n", i, &a[i]);
+    }
+    printf("\n");
+    for(int i=0; i<6; i++){
+        printf("La direccion de b[%i] es: %p\n", i, &b[i]);
+    }
+    printf("\n");
+    for(int i=0; i<6; i++){
+        printf("La direccion de c[%i] es: %p\n", i, &c[i]);
+    }
+    
     return 0;
 }
