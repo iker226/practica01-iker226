8390e7fa53e0726d3dd48c872112f9a570612886
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:22:31 -0600

    Update ej2.c

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -2,5 +2,15 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 2
+    
+    int a[10];
+    
+    printf("&a[4] = %p\n", &a[4]);
+    printf("&a[5] = %p\n", &a[5]);
+    
+    if(&a[4]>&a[5])
+    	printf("La direccion mayor es &a[4]\n");
+    else
+    	printf("La direccion mayor es &a[5]\n");
     return 0;
 }
