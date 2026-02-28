fa76673ca504bcb0e93917635d3218c4cd22b6d6
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:39:45 -0600

    Update ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -2,5 +2,13 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 6
+    
+    int m[2][4];
+    
+    for (int i=0; i<2; i++){
+    	for(int j=0; j<4; j++){
+    		printf("La direccion de m[%d][%d] es: %p\n", i, j, &m[i][j]);
+		}
+	}
     return 0;
 }
