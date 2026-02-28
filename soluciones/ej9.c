636afc84efc3c7d4e3fdb54db4415cb60cd94b7a
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:28:32 -0600

    Update ej9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 9
+    
+    int a[5];
+    
+    printf("&a[-1] = %p\n", &a[-1]);
+    printf("&a[5] = %p\n", &a[5]);
+    
     return 0;
 }
