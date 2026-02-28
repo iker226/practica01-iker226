4a9dabd980051b8c5ef7bfcfd5083e94d841d2e8
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:44:27 -0600

    Update ej11.c

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -2,5 +2,12 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 11
+    
+    double a[3];
+    
+    printf("&a[0] = %p\n", &a[0]);
+    printf("&a[1] = %p\n", &a[1]);
+    printf("&a[2] = %p\n", &a[2]);
+    
     return 0;
 }
