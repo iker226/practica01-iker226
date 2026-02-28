7f3170f06c163e3e52fee7efd2c74a4a8b8d220f
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:41:06 -0600

    Update ej7.c

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 7
+    
+    int m[2][4];
+    
+    printf("&m[0][3] = %p\n", &m[0][3]);
+    printf("&m[1][0] = %p\n", &m[1][0]);
+    
     return 0;
 }
