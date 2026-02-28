8376e7a574344b71b5f7ab7bb9d3009c1af7e17f
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:44:55 -0600

    Update ej12.c

--- soluciones/ej12.c
+++ soluciones/ej12.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 12
+    
+    int m[3][5];
+    
+    printf("&m[2][4] = %p\n", &m[2][4]);
+    printf("&m[1][3] = %p\n", &m[1][3]);
+    
     return 0;
 }
