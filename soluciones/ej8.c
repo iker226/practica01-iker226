4889ada67649bc1a03330f954224e189ba099fd5
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:41:43 -0600

    Update ej8.c

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 8
-    return 0;
+    
+    int m[2][4];
+    
+    printf("&m[0][0] = %p\n", &m[0][0]);
+    printf("&m[1][2] = %p\n", &m[1][2]);
+    
+	return 0;
 }
