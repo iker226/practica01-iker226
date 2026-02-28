25604b18ebbc5aada95437e11f882bdad5ceeeb2
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-02-27 23:38:20 -0600

    Update ej4.c

--- soluciones/ej4.c
+++ soluciones/ej4.c
@@ -2,5 +2,13 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 4
+    int a[10];
+    
+    printf("&a[0] = %p\n", &a[0]);
+    printf("&a[3] = %p\n", &a[3]);
+    printf("&a[5] = %p\n", &a[5]);
+    
+    printf("Tamaño del tipo = %d\n", sizeof(int));
+    
     return 0;
 }
